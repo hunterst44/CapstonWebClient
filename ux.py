60795513717d9149915d6403914c6b77d46b765e
Joel Legassie <joel@jamesbay.org>
2023-09-28 21:57:30 -0700

    Still with the ux

--- ux.py
+++ ux.py
@@ -1,5 +1,5 @@
 import PySimpleGUI as sg
-import socketClient
+import socketClientUx
 import NeuralNetwork
 import oscWriter
 import os.path
@@ -9,9 +9,58 @@ class UX:
 
     def __init__(self, *, theme='BluePurple'):
         self.theme = theme
-        self.writer = oscWriter.OSCWriter()
-        self.dataStream = socketClient.GetData(packetSize=1, label=0, getTraining=False, numSensors=2, packetLimit=1000, pathPreface="data/Orientation01/", writer=self.writer)
+        #self.writer = oscWriter.OSCWriter()
+        #self.dataStream = socketClientUx.GetData(packetSize=1, label=0, getTraining=False, numSensors=2, packetLimit=1000, pathPreface="data/Orientation01/", writer=self.writer)
+        self.packetLimit = 10
+        self.numSensors = 2
+        self.pathPreface = "data/Orientation01/"
+        self.dataTx = 0xFF
+    
+    def trainModels(self, numSensors, packetLimit, pathPreface, *, gestureIdx=-1, labelList=[], labelPathList=[]):
+        #iterate through all the gestures and collect packetLimit samples of each
+        #Called in window 2 and 2.1 where user provides data to set up model and data
+        #Switches to window 3 to output data 
 
+        sampleCount = 0
+        
+        for i in range(gestureIdx):
+            dataStream = socketClientUx.GetData(label=labelList[i], labelPath=labelPathList[i], getTraining=True, numSensors=numSensors, packetLimit=packetLimit, pathPreface=pathPreface)
+            if sampleCount == 0:
+                #dataStream = socketClientUx.GetData(label=label, getTraining=True, numSensors=numSensors, packetLimit=packetLimit, pathPreface=pathPreface)
+                print("First Time?")
+            else:
+                while sampleCount <= packetLimit:
+                    dataStream.getSample()
+                    dataStream.prepTraining()
+                    sampleCount += 1
+        
+        #CSend all the gestures to neural network
+        NeuralNetwork.trainOrientation(pathPreface, labelPathList, 1, numSensors, gestureIdx)
+            
+
+    def predictSample(self, numSensors, pathPreface):
+        dataStream = socketClientUx.GetData(packetSize=1, label=0, getTraining=False, numSensors=numSensors, packetLimit=1000, pathPreface=pathPreface)
+        writer = oscWriter.OSCWriter()
+        dataStream.getSample()
+        predictionList = dataStream.predictSample()
+        writer.getPredictions(predictionList[0])
+
+        #Must get dataTx out of this - and write it into getSample()
+        #Code from old socketClient below
+        # if writer.ToFEnable:
+        #     print(f'Enable Time of Flight Sensor...') 
+        #     self.dataTx = 0 #Reset dataTx
+        #     self.dataTx = struct.pack("=B", 15)   #Enable ToF sensor
+        #     self.extraRxByte = 1
+        # else:
+        #     print(f'Disable Time of Flight Sensor...') 
+        #     self.dataTx = 0 #Reset dataTx
+        #     self.dataTx = struct.pack("=B", 255)   #Disable ToF sensor 
+        #     self.extraRxByte = 0
+        # self.dataGot = 0   #Reset the dataGot flag for the next sample
+
+    
+    
     def makeWindow1(self):
     #Window one welcome, load / create model
         layout = [[sg.Text('The Conductor'), sg.Text(size=(2,1), key='-OUTPUT-')],
@@ -79,6 +128,7 @@ class UX:
                 print("Window 2")
                 if event == sg.WIN_CLOSED or event == 'Exit':
                     break
+
 
         window.close()
 
